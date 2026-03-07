6b684f1d93eecc6bc2e5ac8a003f54913874efff
Jennifer Villamillan <jennifervillamillan11@gmail.com>
2026-03-06 19:53:18 -1200

    se agrega   actualizacion

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,8 +3,8 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         // se crean dos instancias de Persona usando el nuevo constructor
-        Persona p1 = new Persona(12345678, "Juan", "juan@example.com", 5551234, 1990);
-        Persona p2 = new Persona(87654321, "María", "maria@example.com", 5559876, 1985);
+        Persona p1 = new Persona(12345678, "Jennifer", "jennifer@example.com", 5666666, 1996);
+        Persona p2 = new Persona(87654321, "johana", "johana@example.com", 55788555, 1995);
         System.out.println("Datos de persona 1");
         p1.imprimir();
         System.out.println("Datos de persona 2");
