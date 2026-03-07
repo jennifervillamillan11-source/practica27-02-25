1ff6e99d0faf9c838dd16b50528285210d91be6f
Jennifer Villamillan <jennifervillamillan11@gmail.com>
2026-03-06 20:01:17 -1200

    se actualiza  main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,9 +5,9 @@ public class Main {
         // se crean dos instancias de Persona usando el nuevo constructor
         Persona p1 = new Persona(12345678, "Jennifer", "jennifer@example.com", 5666666, 1996);
         Persona p2 = new Persona(87654321, "johana", "johana@example.com", 55788555, 1995);
-        System.out.println("Datos de persona 1");
+        System.out.println("Datos de la persona 1");
         p1.imprimir();
-        System.out.println("Datos de persona 2");
+        System.out.println("Datos de la persona 2");
         p2.imprimir();
 
 }
